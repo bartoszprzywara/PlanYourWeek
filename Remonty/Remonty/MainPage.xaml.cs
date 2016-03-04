db2478d8fb0408934069e724fb64b05e007307f8
BartoszP <przywara.bartosz@gmail.com>
2016-03-04 15:53:15 +0100

    initial flow of the app

--- Remonty/Remonty/MainPage.xaml.cs
+++ Remonty/Remonty/MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -25,6 +26,53 @@ namespace Remonty
         public MainPage()
         {
             this.InitializeComponent();
+            ContentFrame.Navigate(typeof(YourWeek));
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MenuSplitView.IsPaneOpen = !MenuSplitView.IsPaneOpen;
+        }
+
+        private void MenuListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (YourWeekListBoxItem.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(YourWeek));
+                TitleTextBlock.Text = "Twój tydzień";
+                MenuSplitView.IsPaneOpen = false;
+            }
+            else if (IncomingListBoxItem.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(Incoming));
+                TitleTextBlock.Text = "Nowe";
+                MenuSplitView.IsPaneOpen = false;
+            }
+            else if (SettingsListBoxItem.IsSelected)
+            {
+                ContentFrame.Navigate(typeof(Settings));
+                TitleTextBlock.Text = "Ustawienia";
+                MenuSplitView.IsPaneOpen = false;
+            }
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            SearchSplitView.IsPaneOpen = !SearchSplitView.IsPaneOpen;
+            SearchSplitTextBox.Text = String.Empty;
+        }
+
+        private void SearchSplitButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(SearchResults), SearchSplitTextBox.Text);
+            TitleTextBlock.Text = "Wyniki";
+            SearchSplitView.IsPaneOpen = false;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigate(typeof(AddActivity));
+            TitleTextBlock.Text = "Nowe zadanie";
         }
     }
 }
